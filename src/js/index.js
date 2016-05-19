c8871348444aa480fa2e5affd2f85c0d091a1a5c
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-05-19 15:54:47 -0400

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+// Javascript Entry Point
+import { Component } from 'react';
+
+
+
+
+export default class Main extends Component {
+	render () {
+		return (
+			<div className="main-wrapper">
+				<header>
+				</header>
+
+
+
+
+
+
+
+
+			</div>
+
+
+
+
+
+			)
+	}
+}
