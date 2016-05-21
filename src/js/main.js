264196d40078ebca6b2389d793c6481dc6ebcff0
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-05-21 14:04:06 -0400

    added "about me" section and css.

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,17 @@ export default class Main extends Component {
 				<div className="home-image">
 				<h1>Let's get there together.</h1>
 				</div>
+				<div id="projects" className="project-wrapper">
+				<div className="about-me">
+					<div className="tagline">
+						<ul>
+							<li><img src="../images/Josh Diboll-06 2015 Headshots at Lenox-0038.jpg"/></li>
+							<li><p>Hi, my name is Joshua. I create <strong>functional</strong> web applications 
+							that make the complex, <strong>simple.</strong> </p></li>
+						</ul>
+					</div>
+				</div>
+				</div>
 				<footer>
 					<Link to="/"><i className="fa fa-github"></i></Link>
 					<Link to="/"><i className="fa fa-linkedin" aria-hidden="true"></i></Link>
