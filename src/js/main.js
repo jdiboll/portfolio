22b7f19e3084be1969925d2ae07a727fc67f2835
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-05-21 15:19:30 -0400

    added styling and began plotting project section.

--- src/js/main.js
+++ src/js/main.js
@@ -21,21 +21,30 @@ export default class Main extends Component {
 				<div className="home-image">
 				<h1>Let's get there together.</h1>
 				</div>
-				<div id="projects" className="project-wrapper">
 				<div className="about-me">
 					<div className="tagline">
 						<ul>
 							<li><img src="../images/Josh Diboll-06 2015 Headshots at Lenox-0038.jpg"/></li>
 							<li><p>Hi, my name is Joshua. I create <strong>functional</strong> web applications 
-							that make the complex, <strong>simple.</strong> </p></li>
+							that make the complex, <strong>simple.</strong>I'm passionate about the
+							 human connection and constantly curious why we do what we do.</p></li>
 						</ul>
 					</div>
 				</div>
+				<div id="projects" className="project-wrapper">
+					<div className="final-project"></div>
+					<div className="hackathon"></div>
+					<div className="iron-pics"></div>
+					<div className="ye-olde"></div>
+					<div className="first-project"></div>
 				</div>
 				<footer>
-					<Link to="/"><i className="fa fa-github"></i></Link>
-					<Link to="/"><i className="fa fa-linkedin" aria-hidden="true"></i></Link>
-					<Link to="/"><i className="fa fa-medium" aria-hidden="true"></i></Link>
+					<ul>
+						<li><a href="https://github.com/jdiboll"><i className="fa fa-github"></i></a></li>
+						<li><a href="https://www.linkedin.com/profile/preview?locale=en_US&trk=prof-0-sb-preview-primary-button"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						<li><a href="https://medium.com/@jdiboll"><i className="fa fa-medium" aria-hidden="true"></i></a></li>
+						<li><a href="https://twitter.com/jdiboll233"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+					</ul>
 				</footer>
 			</div>
 			)
